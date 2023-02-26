561d408b87ecc80fc2dc6a3e48799808c84dd819
echo <echo.backend@gmail.com>
2023-02-26 09:23:18 +0900

    chore: 에러 메세지 표시

--- gugles_mission_tracker/app.py
+++ gugles_mission_tracker/app.py
@@ -3,10 +3,20 @@ import notify,update,arg_parser
 def run():
     args = arg_parser.parse_args()
     if args.action == "check":
-        update.update_cells("this_week")
-    else:
-        update.update_cells("last_week")
-        notify.make_notification("last_week")
+        try:
+            update.update_cells("this_week")
+            print("미션 수행 결과를 업데이트했습니다.")
+        except Exception as e:
+            print("미션 수행 결과를 업데이트하는데 실패했습니다.")
+            print(e)
+    
+    elif args.action == "report":
+        try:
+            update.update_cells("last_week")
+            notify.make_notification("last_week")
+            print("미션 수행 결과를 업데이트하고 알림을 보냈습니다.")
+        except Exception as e:
+            print("미션 수행 결과를 업데이트하거나 알림을 보내는데 실패했습니다.")
     
 
 if __name__ == "__main__":
