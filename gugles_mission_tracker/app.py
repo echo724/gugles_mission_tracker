0db4c95a30a496e5a7f00455434668fea7d7416b
echo <echo.backend@gmail.com>
2023-07-03 00:34:33 +0900

    feat: 반성문과 별도로 공지하는 명령어 추가

--- gugles_mission_tracker/app.py
+++ gugles_mission_tracker/app.py
@@ -26,6 +26,16 @@ def run():
             print("미션 수행 결과를 업데이트했습니다.")
             notify.make_notification("last_week")
             print("미션 수행 결과 알림을 보냈습니다.")
+        except Exception as e:
+            print("미션 수행 결과를 업데이트하거나 알림을 보내는데 실패했습니다.")
+            print(e)
+    elif args.action == "report_with_apology":
+        try:
+            status = status_maker.get_status("last_week")
+            sheets.update_cell(status, "last_week")
+            print("미션 수행 결과를 업데이트했습니다.")
+            notify.make_notification("last_week")
+            print("미션 수행 결과 알림을 보냈습니다.")
             notify.make_apology_notification()
             print("반성문 작성하지 않은 크루 알림을 보냈습니다.")
         except Exception as e:
